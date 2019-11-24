c503fd2992987dc56ac9f394d7eca0b97711cfed
Nguyen Hai Dang <mr.pi.cbg@gmail.com>
2019-11-25 03:26:49 +0700

    Bars' buttons DONE

--- JulieUI/Dashboard.Designer.cs
+++ JulieUI/Dashboard.Designer.cs
@@ -32,14 +32,20 @@
             this.panelTopBar = new System.Windows.Forms.Panel();
             this.textBoxSearch = new System.Windows.Forms.TextBox();
             this.buttonCloseWindow = new System.Windows.Forms.Button();
-            this.buttonBack = new System.Windows.Forms.Button();
+            this.buttonMinimize = new System.Windows.Forms.Button();
             this.panel1 = new System.Windows.Forms.Panel();
             this.panelLeftBar = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.labelLogo = new System.Windows.Forms.Label();
+            this.buttonProduct = new System.Windows.Forms.Button();
+            this.buttonHome = new System.Windows.Forms.Button();
             this.buttonCategory = new System.Windows.Forms.Button();
+            this.panelUserControls = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelStudentID = new System.Windows.Forms.Label();
             this.panelTopBar.SuspendLayout();
             this.panelLeftBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panelTopBar
@@ -47,12 +53,12 @@
             this.panelTopBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
             this.panelTopBar.Controls.Add(this.textBoxSearch);
             this.panelTopBar.Controls.Add(this.buttonCloseWindow);
-            this.panelTopBar.Controls.Add(this.buttonBack);
+            this.panelTopBar.Controls.Add(this.buttonMinimize);
             this.panelTopBar.Controls.Add(this.panel1);
             this.panelTopBar.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panelTopBar.Location = new System.Drawing.Point(0, 0);
+            this.panelTopBar.Location = new System.Drawing.Point(170, 0);
             this.panelTopBar.Name = "panelTopBar";
-            this.panelTopBar.Size = new System.Drawing.Size(800, 32);
+            this.panelTopBar.Size = new System.Drawing.Size(730, 32);
             this.panelTopBar.TabIndex = 0;
             // 
             // textBoxSearch
@@ -60,42 +66,42 @@
             this.textBoxSearch.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
             this.textBoxSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBoxSearch.Font = new System.Drawing.Font("Candara Light", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBoxSearch.ForeColor = System.Drawing.SystemColors.ScrollBar;
-            this.textBoxSearch.Location = new System.Drawing.Point(150, 3);
+            this.textBoxSearch.ForeColor = System.Drawing.SystemColors.Window;
+            this.textBoxSearch.Location = new System.Drawing.Point(3, 3);
             this.textBoxSearch.Name = "textBoxSearch";
             this.textBoxSearch.Size = new System.Drawing.Size(270, 27);
-            this.textBoxSearch.TabIndex = 1;
+            this.textBoxSearch.TabIndex = 2;
             this.textBoxSearch.Text = "Enter product name to search...";
             // 
             // buttonCloseWindow
             // 
             this.buttonCloseWindow.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
             this.buttonCloseWindow.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonCloseWindow.BackgroundImage")));
-            this.buttonCloseWindow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonCloseWindow.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.buttonCloseWindow.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonCloseWindow.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
-            this.buttonCloseWindow.Location = new System.Drawing.Point(751, 2);
+            this.buttonCloseWindow.Location = new System.Drawing.Point(693, 0);
             this.buttonCloseWindow.Margin = new System.Windows.Forms.Padding(0);
             this.buttonCloseWindow.Name = "buttonCloseWindow";
-            this.buttonCloseWindow.Size = new System.Drawing.Size(44, 28);
+            this.buttonCloseWindow.Size = new System.Drawing.Size(36, 32);
             this.buttonCloseWindow.TabIndex = 0;
             this.buttonCloseWindow.TabStop = false;
             this.buttonCloseWindow.UseVisualStyleBackColor = false;
             // 
-            // buttonBack
+            // buttonMinimize
             // 
-            this.buttonBack.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
-            this.buttonBack.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonBack.BackgroundImage")));
-            this.buttonBack.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.buttonBack.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.buttonBack.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
-            this.buttonBack.Location = new System.Drawing.Point(705, 2);
-            this.buttonBack.Margin = new System.Windows.Forms.Padding(0);
-            this.buttonBack.Name = "buttonBack";
-            this.buttonBack.Size = new System.Drawing.Size(44, 28);
-            this.buttonBack.TabIndex = 0;
-            this.buttonBack.TabStop = false;
-            this.buttonBack.UseVisualStyleBackColor = false;
+            this.buttonMinimize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.buttonMinimize.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonMinimize.BackgroundImage")));
+            this.buttonMinimize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.buttonMinimize.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonMinimize.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(33)))), ((int)(((byte)(43)))));
+            this.buttonMinimize.Location = new System.Drawing.Point(657, 0);
+            this.buttonMinimize.Margin = new System.Windows.Forms.Padding(0);
+            this.buttonMinimize.Name = "buttonMinimize";
+            this.buttonMinimize.Size = new System.Drawing.Size(36, 32);
+            this.buttonMinimize.TabIndex = 0;
+            this.buttonMinimize.TabStop = false;
+            this.buttonMinimize.UseVisualStyleBackColor = false;
             // 
             // panel1
             // 
@@ -107,58 +113,125 @@
             // panelLeftBar
             // 
             this.panelLeftBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(33)))));
+            this.panelLeftBar.Controls.Add(this.labelStudentID);
+            this.panelLeftBar.Controls.Add(this.labelName);
+            this.panelLeftBar.Controls.Add(this.pictureBox1);
+            this.panelLeftBar.Controls.Add(this.labelLogo);
+            this.panelLeftBar.Controls.Add(this.buttonProduct);
+            this.panelLeftBar.Controls.Add(this.buttonHome);
             this.panelLeftBar.Controls.Add(this.buttonCategory);
             this.panelLeftBar.Dock = System.Windows.Forms.DockStyle.Left;
-            this.panelLeftBar.Location = new System.Drawing.Point(0, 32);
+            this.panelLeftBar.Location = new System.Drawing.Point(0, 0);
             this.panelLeftBar.Name = "panelLeftBar";
-            this.panelLeftBar.Size = new System.Drawing.Size(150, 546);
+            this.panelLeftBar.Size = new System.Drawing.Size(170, 500);
             this.panelLeftBar.TabIndex = 1;
             // 
-            // panel2
+            // labelLogo
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(47)))), ((int)(((byte)(61)))));
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(0, 578);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(800, 22);
-            this.panel2.TabIndex = 2;
+            this.labelLogo.AutoSize = true;
+            this.labelLogo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelLogo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 32.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelLogo.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelLogo.Location = new System.Drawing.Point(3, 3);
+            this.labelLogo.Name = "labelLogo";
+            this.labelLogo.Size = new System.Drawing.Size(164, 53);
+            this.labelLogo.TabIndex = 4;
+            this.labelLogo.Text = " JULIE ";
             // 
-            // panel3
+            // buttonProduct
             // 
-            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(197)))), ((int)(((byte)(201)))));
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(150, 32);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(650, 546);
-            this.panel3.TabIndex = 3;
+            this.buttonProduct.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonProduct.BackgroundImage")));
+            this.buttonProduct.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonProduct.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonProduct.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(33)))));
+            this.buttonProduct.Location = new System.Drawing.Point(0, 220);
+            this.buttonProduct.Name = "buttonProduct";
+            this.buttonProduct.Size = new System.Drawing.Size(170, 40);
+            this.buttonProduct.TabIndex = 3;
+            this.buttonProduct.UseVisualStyleBackColor = true;
+            // 
+            // buttonHome
+            // 
+            this.buttonHome.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonHome.BackgroundImage")));
+            this.buttonHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.buttonHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonHome.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(33)))));
+            this.buttonHome.Location = new System.Drawing.Point(0, 132);
+            this.buttonHome.Name = "buttonHome";
+            this.buttonHome.Size = new System.Drawing.Size(170, 40);
+            this.buttonHome.TabIndex = 1;
+            this.buttonHome.UseVisualStyleBackColor = true;
             // 
             // buttonCategory
             // 
             this.buttonCategory.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("buttonCategory.BackgroundImage")));
-            this.buttonCategory.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.buttonCategory.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
             this.buttonCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonCategory.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(14)))), ((int)(((byte)(22)))), ((int)(((byte)(33)))));
-            this.buttonCategory.Location = new System.Drawing.Point(1, 39);
+            this.buttonCategory.Location = new System.Drawing.Point(0, 176);
             this.buttonCategory.Name = "buttonCategory";
-            this.buttonCategory.Size = new System.Drawing.Size(150, 48);
-            this.buttonCategory.TabIndex = 0;
+            this.buttonCategory.Size = new System.Drawing.Size(170, 40);
+            this.buttonCategory.TabIndex = 2;
             this.buttonCategory.UseVisualStyleBackColor = true;
+            // 
+            // panelUserControls
+            // 
+            this.panelUserControls.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(186)))), ((int)(((byte)(197)))), ((int)(((byte)(201)))));
+            this.panelUserControls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelUserControls.Location = new System.Drawing.Point(170, 32);
+            this.panelUserControls.Name = "panelUserControls";
+            this.panelUserControls.Size = new System.Drawing.Size(730, 468);
+            this.panelUserControls.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(3, 449);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelName.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelName.Location = new System.Drawing.Point(50, 456);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(117, 15);
+            this.labelName.TabIndex = 6;
+            this.labelName.Text = "NGUYEN HAI DANG";
+            // 
+            // labelStudentID
+            // 
+            this.labelStudentID.AutoSize = true;
+            this.labelStudentID.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStudentID.ForeColor = System.Drawing.SystemColors.Window;
+            this.labelStudentID.Location = new System.Drawing.Point(50, 476);
+            this.labelStudentID.Name = "labelStudentID";
+            this.labelStudentID.Size = new System.Drawing.Size(74, 15);
+            this.labelStudentID.TabIndex = 7;
+            this.labelStudentID.Text = "ID: 1988010";
             // 
             // Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.ClientSize = new System.Drawing.Size(900, 500);
             this.ControlBox = false;
-            this.Controls.Add(this.panel3);
-            this.Controls.Add(this.panelLeftBar);
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelUserControls);
             this.Controls.Add(this.panelTopBar);
+            this.Controls.Add(this.panelLeftBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Dashboard";
             this.panelTopBar.ResumeLayout(false);
             this.panelTopBar.PerformLayout();
             this.panelLeftBar.ResumeLayout(false);
+            this.panelLeftBar.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -169,11 +242,16 @@
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Panel panelLeftBar;
         private System.Windows.Forms.Button buttonCloseWindow;
-        private System.Windows.Forms.Button buttonBack;
+        private System.Windows.Forms.Button buttonMinimize;
         private System.Windows.Forms.TextBox textBoxSearch;
-        private System.Windows.Forms.Panel panel2;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelUserControls;
         private System.Windows.Forms.Button buttonCategory;
+        private System.Windows.Forms.Button buttonHome;
+        private System.Windows.Forms.Button buttonProduct;
+        private System.Windows.Forms.Label labelLogo;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelStudentID;
+        private System.Windows.Forms.Label labelName;
     }
 }
 
